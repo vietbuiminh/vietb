f2be8a592d6b83f6025b7ad0afb68dc62c7000ea
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-03 15:12:16 -0500

    Changed the navbar

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import { Carousel } from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark" className="my-nav">
+      <Navbar bg="light" variant="light" className="my-nav">
         <Navbar.Brand href="#home">
-          Viet Bui Websit
+          Viet Bui Web!
         </Navbar.Brand>
       </Navbar>
       <Container>
@@ -34,33 +34,34 @@ function App() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Nullam sed maximus eros, id elementum dolor. Phasellus ultrices ante nec elementum placerat. Ut vestibulum feugiat turpis a elementum. Phasellus congue augue sit amet vehicula ultricies. Nulla rutrum tellus sit amet iaculis egestas. Sed id urna scelerisque, tristique odio nec, feugiat lorem. Sed tincidunt, turpis tempus suscipit aliquet, orci elit viverra urna, at cursus justo odio et ligula. Nulla ut lectus eu ligula tempus viverra. Donec bibendum tempor facilisis. Nam in neque molestie justo semper mattis vitae in lorem. Curabitur commodo risus ex, a porttitor arcu luctus non. Nunc luctus, lacus eu dictum lacinia, arcu quam sagittis est, eu blandit ligula ligula et dolor. Pellentesque luctus augue ut nisl cursus faucibus. Vestibulum consequat finibus hendrerit.</p>
             <p>Proin viverra lacinia tellus, in efficitur ante faucibus vitae. Ut accumsan tincidunt diam. Donec suscipit maximus dolor, eu dictum sem sodales a. In maximus, orci eu gravida ullamcorper, augue tortor facilisis neque, non ultricies risus est id augue. Duis placerat convallis justo, et rhoncus tortor tempus a. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Proin interdum tellus nulla, non feugiat urna suscipit in. Etiam dignissim nibh sit amet ligula tincidunt, nec pretium ante efficitur. Nunc quis erat ut orci fermentum dignissim. Donec aliquam, urna nec vulputate vestibulum, elit dolor lacinia urna, vitae molestie magna elit efficitur augue. Integer dolor est, tempus in nulla nec, faucibus lobortis libero. Nullam sodales volutpat risus a maximus. Nulla iaculis est felis, sit amet luctus lorem fringilla ut. Ut in lorem dui. Donec aliquam in massa a luctus.</p>
-            <h1>LHDL: Share Top 3 Gilds</h1>
+            <h1>Top Programming Languages</h1>
             <Table striped bordered hover>
               <thead>
                 <tr>
                   <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>Username</th>
+                  <th>Language</th>
+                  <th>Level</th>
+                  <th>Years</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>1</td>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td>Python🐍</td>
+                  <td>Intermediate</td>
+                  <td>2</td>
                 </tr>
                 <tr>
                   <td>2</td>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
+                  <td>Java</td>
+                  <td>Intermediate</td>
+                  <td>1</td>
                 </tr>
                 <tr>
                   <td>3</td>
-                  <td colSpan="2">Larry the Bird</td>
-                  <td>@twitter</td>
+                  <td>Pascal</td>
+                  <td>Advanced</td>
+                  <td>3</td>
                 </tr>
               </tbody>
             </Table>
