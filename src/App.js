f1e846d2f9a0a9ddd29ef7dd6c934112da051b4e
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-03 15:29:06 -0500

    Fixed issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import Navbar from "react-bootstrap/NavBar"
-import { CarouselItem, Col, Container, Row, Table } from 'react-bootstrap';
+import { CarouselItem, Col, Container, Row, Table, Navbar } from 'react-bootstrap';
 import Card from "react-bootstrap/Card"
 import { Carousel } from 'react-bootstrap';
 
@@ -71,7 +70,7 @@ function App() {
               <Card.Body>
                 <Card.Title>Viet Bui</Card.Title>
                 <Card.Subtitle className="mb-2">Newbie with React</Card.Subtitle>
-                <img src="https://lh6.googleusercontent.com/2yhYAJnKvWNr0I-OfxsWmB57mMQWEewjCTLP3FxJm1VJiTN0oOr6I9LMZCETdtd_wtUepgOs8cW92-b6-aBVXh1Kg7hA-yHPIOQmYHVRtf1WtmCfo2BYy7Sh3Ewydzq_lQ=w1280" alt="Image of myself."/>
+                <img src="https://lh6.googleusercontent.com/2yhYAJnKvWNr0I-OfxsWmB57mMQWEewjCTLP3FxJm1VJiTN0oOr6I9LMZCETdtd_wtUepgOs8cW92-b6-aBVXh1Kg7hA-yHPIOQmYHVRtf1WtmCfo2BYy7Sh3Ewydzq_lQ=w1280" alt="the capture of myself."/>
                 <Card.Text>
                   Viet Bui is a current college student in the US. He originally from Saigon and very new the life over here.
                 </Card.Text>
