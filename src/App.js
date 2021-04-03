26ae4b112ef9d6092373c974a4d67a75f365345d
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-03 11:37:34 -0500

    Added grid

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Navbar from "react-bootstrap/NavBar"
+import { Col, Container, Row } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">
-          Viet Bui Website
+          Viet Bui Websit
         </Navbar.Brand>
       </Navbar>
+      <Container>
+        <Row>
+          <Col xs={12} md={8}>...</Col>
+          <Col xs={12} md={4}>...</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
