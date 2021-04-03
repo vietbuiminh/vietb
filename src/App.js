71bc497b3b247542dcc170433d30ba581dccdfce
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-03 11:28:58 -0500

    Added a nav bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
+import Navbar from "react-bootstrap/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello People.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">
+          Viet Bui Website
+        </Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
